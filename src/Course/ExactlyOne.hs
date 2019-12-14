4272e293db4ca9736f21c11e2bd78759d78b2558
hduser <a.b@c.com>
2019-12-14 02:20:17 -0500

    Added comments on ExactlyOne

--- src/Course/ExactlyOne.hs
+++ src/Course/ExactlyOne.hs
@@ -13,16 +13,27 @@ data ExactlyOne a = ExactlyOne a deriving (Eq, Show)
 runExactlyOne :: ExactlyOne a -> a
 runExactlyOne (ExactlyOne a) = a
 
+-- | like map in scala
 mapExactlyOne :: (a -> b) -> ExactlyOne a -> ExactlyOne b
 mapExactlyOne f (ExactlyOne a)    = ExactlyOne (f a)
 
+-- | P.fmap (+3) (ExactlyOne 10)
+-- return ExactlyOne 13
 bindExactlyOne :: (a -> ExactlyOne b) -> ExactlyOne a -> ExactlyOne b
 bindExactlyOne f (ExactlyOne a) = f a
 
+-- | (ExactlyOne (\n -> (n + 3))) A.<*> (ExactlyOne 10)
+-- return ExactlyOne 13
+-- | (ExactlyOne (+3)) A.<*> (ExactlyOne 10)
+-- return ExactlyOne 13
 instance P.Functor ExactlyOne where
   fmap =
     M.liftM
 
+-- | (\n -> (ExactlyOne (n+3))) M.=<< (ExactlyOne 10)
+-- return ExactlyOne 13
+-- | (ExactlyOne 10) M.>>= (\n -> (ExactlyOne (n+3)))
+-- return ExactlyOne 13
 instance A.Applicative ExactlyOne where
   (<*>) =
     M.ap
